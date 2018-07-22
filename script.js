8063bcb8d00bc2ad1562d26bed988fc4e2222d88
paulrunco <39491963+paulrunco@users.noreply.github.com>
2018-07-21 22:39:52 -0400

    Changes
    Added footer
    Updated heading
    Added date fields

--- script.js
+++ script.js
@@ -1,61 +1,99 @@
 $(document).ready(function(){
     addQuoteLine(); // add first row to quote grid
+    reNumber(); // number the first rows
 
+    // Add rows using button at bottom of page
    $("#add_row").click(function(){
        addQuoteLine();
+       reNumber(); // number rows
    });
 
-   $('#quote-grid').on('blur', '.unit-price-input', function() {
-      alert('Action!')
+   $('#quote-grid').on('change', '.update-price', function() {
+       reCalculateNet();
+      //alert($(this).closest('div.quote-line').attr("id"))
+      //$('p.net-price').text("4");
    });
 
-   $('#del_row').click(function(){
-       // delete a row
+   // Delete quote lines when link is clicked
+   $('#quote-grid').on('click','.del-line', function(){
+       $(this).closest('.quote-line').remove();
+       reNumber();
    });
 
 });
 
+function reCalculateNet() {
+    $('tr.calculate').each(function(i){
+        var line = $(this);
+        var qty = line.find("[name=qty]").val();
+        var price = line.find("[name=price]").val();
+        if (price != "") {
+            line.find("[name=price]").val(parseFloat(price).toFixed(2));
+        }
+        if (qty == "" | price == "") {
+            line.find(('p.net-price')).text("$0.00");
+        }
+        else {
+            var subtotal = parseInt(qty,10) * parseFloat(price);
+            line.find(('p.net-price')).text("$" + subtotal.toFixed(2));
+        }
+    });
+}
+
+function reNumber() {
+    $('td.line-number').each(function(i){
+        $(this).text(i+1);
+    });
+}
+
 function addQuoteLine() {
     // count number of existing rows
     var lineNum = $(".quote-line").length + 1;
-    var partLine = ['<div class="quote-line" id="line'+lineNum+'">',
+    var partLine = ['<div class="quote-line">',
                         '<div class="pnum-line">',
                             '<table class="pnum-line-table">',
-                                '<tr>',
-                                    '<th>Line</th>',
-                                    '<th>Part Number</th>',
-                                    '<th>Description</th>',
-                                    '<th>Rev</th>',
-                                    '<th>Drawing</th>',
-                                '</tr>',
-                                '<tr>',
-                                    '<td>'+lineNum+'</td>',
-                                    '<td><input type="text" size="20"></td>',
-                                    '<td><input type="text" size="40"></td>',
-                                    '<td><input type="text" size="4"></td>',
-                                    '<td><input type="text" size="20"></td>',
-                                '</tr>',
+                                '<thead>',
+                                    '<tr>',
+                                        '<td>Line</td>',
+                                        '<td>Part Number</td>',
+                                        '<td>Description</td>',
+                                        '<td>Rev</td>',
+                                        '<td>Drawing</td>',
+                                    '</tr>',
+                                '</thead>',
+                                '<tbody>',
+                                    '<tr>',
+                                        '<td class="line-number"></td>',
+                                        '<td><input type="text" size="20"></td>',
+                                        '<td><input type="text" size="40"></td>',
+                                        '<td><input type="text" size="4"></td>',
+                                        '<td><input type="text" size="20"></td>',
+                                    '</tr>',
+                                '<tbody>',
                             '</table>',
                         '</div><!--.pnum-line -->'].join('');
     var lineComment = ['<div class="line-comment"><textarea placeholder="Line comments..." ></textarea></div>']
     var qtyLine = ['<div class="qty-line">',
                         '<div class="qty-line-left"></div>',
                         '<div class="qty-line-right"><table class="qty-line-table">',
-                            '<tr>',
-                                '<th>Quantity</th>',
-                                '<th>Unit Price</th>',
-                                '<th>Net Price</th>',
-                            '</tr>',
-                            '<tr>',
-                                '<td><input type="number" class="qty-input"></td>',
-                                '<td><input type="number" class="unit-price-input" min="0.01" step="0.01"></td>',
-                                '<td><p class="net-price"></p></td>',
-                            '</tr>',
+                            '<thead>',
+                                '<tr>',
+                                    '<td>Quantity</td>',
+                                    '<td>Unit Price</td>',
+                                    '<td>Net Price</td>',
+                                '</tr>',
+                            '</thead>',
+                            '<tbody>',
+                                '<tr class="calculate">',
+                                    '<td><input type="number" name="qty" class="update-price"></td>',
+                                    '<td><input type="number" name="price" class="update-price"></td>',
+                                    '<td><p class="net-price">$0.00</p></td>',
+                                '</tr>',
+                            '</tbody>',
                         '</table></div><!--.qty-line-right-->',
                     '</div><!--.qty-line-->',
-                    '<div class="quote-line-controls"><a href="#" onclick="return false;" class="del-row">Delete Row</a></div>',
+                    '<div class="quote-line-controls"><a href="#" onclick="return false;" class="del-line">Delete Line</a></div>',
                 '</div><!--.quote-line -->'].join('');
 
         $('#quote-grid').append(partLine + lineComment + qtyLine);
-
 }
